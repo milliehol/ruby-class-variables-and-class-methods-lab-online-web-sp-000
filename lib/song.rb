e9a4cb39ab7c4128987c3e91ec805e5e9ca134de
Carla Holcomb <carla.programmer@gmail.com>
2019-11-02 16:16:57 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+
+def Song
+
+  attr_accessor :color, :size, :material, :condition	  
+  attr_reader :brand	  
+
+
+  BRANDS = []
+
+  def initialize(brand)	  
+    @brand = brand	    
+    BRANDS << brand
+    BRANDS == BRANDS.uniq!
+  end	  
+
+end
